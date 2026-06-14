d810443a99631227178c6a621cd1e2f56d806dd9
alansmello <alansmello@gmail.com>
2026-06-14 11:12:00 -0300

    feat: implementa Phase 3: Infra.Data - Leitura agregada

--- src/Amani.ImportadosERP.Application/Interfaces/IEstoqueConsultaRepository.cs
+++ src/Amani.ImportadosERP.Application/Interfaces/IEstoqueConsultaRepository.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Amani.ImportadosERP.Application.DTOs;
+using Amani.ImportadosERP.Domain.Entities;
 
 namespace Amani.ImportadosERP.Application.Interfaces;
 
 public interface IEstoqueConsultaRepository
 {
     Task<int> ObterSaldoAsync(Guid produtoId);
+    Task<IReadOnlyCollection<EstoqueProdutoSaldoDto>> ObterSaldosAsync(Guid? categoriaId, bool apenasComSaldo);
+    Task<IReadOnlyCollection<EstoqueMovimentacaoItemDto>> ObterMovimentacoesAsync(Guid produtoId, DateTime? dataInicio, DateTime? dataFim, TipoMovimentacao? tipo, int limite);
+    Task<int> ContarMovimentacoesAsync(Guid produtoId, DateTime? dataInicio, DateTime? dataFim, TipoMovimentacao? tipo);
 }
