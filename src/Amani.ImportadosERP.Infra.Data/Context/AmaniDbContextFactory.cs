9c7d5f306a971f3a069897e575ab0116e620604c
alansmello <alansmello@gmail.com>
2026-06-28 10:56:36 -0300

    Ajusta DbContextFactory para usar connection string por ambiente

--- src/Amani.ImportadosERP.Infra.Data/Context/AmaniDbContextFactory.cs
+++ src/Amani.ImportadosERP.Infra.Data/Context/AmaniDbContextFactory.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Amani.ImportadosERP.Infra.Data.Context;
 
@@ -12,15 +8,14 @@ public class AmaniDbContextFactory : IDesignTimeDbContextFactory<AmaniDbContext>
 {
     public AmaniDbContext CreateDbContext(string[] args)
     {
+        var connectionString =
+            Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+            ?? "Host=localhost;Port=5433;Database=amani_db;Username=postgres;Password=surf22bob";
+
         var optionsBuilder = new DbContextOptionsBuilder<AmaniDbContext>();
 
-        optionsBuilder.UseNpgsql(
-            "Host=localhost;Port=5433;Database=amani_db;Username=postgres;Password=surf22bob"
-        );
+        optionsBuilder.UseNpgsql(connectionString);
 
         return new AmaniDbContext(optionsBuilder.Options);
-
     }
-
-   
 }
